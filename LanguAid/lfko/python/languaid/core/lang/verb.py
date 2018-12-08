2d83a03808cdcae8b95638f79da80331367b76c3
lfko <lfko@Sartre>
2018-12-08 12:38:59 +0100

    vowel harmonization finished

--- LanguAid/lfko/python/languaid/core/lang/verb.py
+++ LanguAid/lfko/python/languaid/core/lang/verb.py
@@ -12,14 +12,18 @@ class Verb():
     def __init__(self):
         """ """
         self.rl = RuleLoader()
+        # actual turkish vowels
         self.vowels = ['a', 'e', 'i', 'o', 'u', 'ö', 'ü']
 
-    def construct(self, word='altınlamak', args=[]):
+    def construct(self, word, args=[]):
         """ 
             @param **kwargs: key-value-params
         """
-        args = [('mode', 'negation'),
-                ('tense', 'present'), ('person', 'type1', 5)]
+
+        # TODO check arguments
+
+        #args = [('mode', 'negation'),
+        #        ('tense', 'present'), ('person', 'type1', 5)]
         # at first, read the args dict and identify, which rules should be
         # loaded (e.g. for a specific tense)
         # the basic build rule: replace the suffix of the infinitive
@@ -45,35 +49,39 @@ class Verb():
                     # print(suffix)
             buildRule = buildRule + " + " + "'" + suffix + "'"
 
+        # with eval, we are able to execute a string as actual python code
         evalBuild = eval("'" + word_stem + "'" + buildRule)
-        print(evalBuild)
 
-        self.__harmonizeVocals__(evalBuild, word_stem)
+        return self.__harmonizeVowels__(evalBuild, word_stem)
 
     def deconstruct(self, args):
         """ """
 
-    def __harmonizeVocals__(self, word, word_stem):
+    def __harmonizeVowels__(self, word, word_stem):
         """ """
         preceding_vow = [c for c in word_stem if c in self.vowels][-1]
-        print(preceding_vow)
-        word2 = word.replace(word_stem, '')
-        print(word2)
+        # create a copy of the word, reduced by the word stem
+        # strings in python are immutables
+        word = word.replace(word_stem, '')
 
+        # get the dictionary of high vowels
         high_vow = self.rl.find(('vowel_harmony', 'high_vowels'))
-        print('high: ', high_vow)
-        word = [c for c in word if c in high_vow]
+        # get the dictionary of low vowels
         low_vow = self.rl.find(('vowel_harmony', 'low_vowels'))
-        print('low: ', low_vow)
+
+        # since strings are immutable, we manage a list of replacements, which
+        # will be applied afterwards to the word
         word_repl = {}
-        for i, c in enumerate(word2):
+        for i, c in enumerate(word):
 
             print('current index {} and char {}'.format(i, c))
-            if c in self.vowels or c == '_':
+            if c in self.vowels or c in ['-', '_']:
                 # char is a valid vowel
-                if c != preceding_vow and word2[(i - 1)] != '*':
+                if c != preceding_vow and word[(i - 1)] != '*':
                     if c == '_':
                         cNew = high_vow[preceding_vow]
+                    elif c == '-':
+                        cNew = low_vow[preceding_vow]
                     else:
                         cNew = high_vow[c]
 
@@ -85,13 +93,17 @@ class Verb():
 
                     print('setting new vowel')
 
-        print(word_repl)
-        word_as_list = list(word2)
+        # get the word as a list, so we can replace characters (i.e. vowels)
+        word_as_list = list(word)
         for key in word_repl.keys():
             word_as_list[key] = word_repl[key]
 
+        # final word
+        word = word_stem + ''.join(word_as_list)
         print(word_stem + ''.join(word_as_list))
 
+        return word
 
-vb = Verb()
-vb.construct()
+
+#vb = Verb()
+#vb.construct()
