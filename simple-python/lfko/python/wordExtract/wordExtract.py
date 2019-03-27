6c0f55daeec3f0b7fb5d74fea82a2e6099f65b85
Florian Becker <31791282+lfko@users.noreply.github.com>
2019-03-27 15:42:30 +0100

    latest version, printing out tab separated results

--- simple-python/lfko/python/wordExtract/wordExtract.py
+++ simple-python/lfko/python/wordExtract/wordExtract.py
@@ -7,80 +7,6 @@ import re
 import csv
 import pandas as pd
 
-
-def extractWord(row):
-    '''
-    '''
-    # print('row before: ', row)
-    # pattern = re.compile(r'[\u00C0-\u017Fa-zA-Z0-9]*[\u00C0-\u017Fa-zA-Z][\u00C0-\u017Fa-zA-Z0-9]*')
-    # pattern = re.compile(r'(\W*\s[0-9]+){1}')
-    # pattern to find only number-strings; they shall be excluded
-    pattern = re.compile(r'(\s{2}[0-9][0-9]+){1}')
-    word = pattern.findall(row)
-    ret = []
-
-    if(len(word) > 0):
-        for match in word:
-            row = row.replace(match, '')
-            # print('row after: ', row)
-            # ret = row.split()
-    # else:
-        # ret = row.split()
-
-    # pattern = re.compile(r'(["]{1}[\w|\s]*["]{1})')
-    '''
-        find words, which are enclosed with quotation marks (e.g. "Foo Bar Faz")
-        they will be extracted and removed afterwards from the string
-    '''
-    # pattern = re.compile(r'(["]{1}[\w]+[\w|\s]+["]{1})', re.UNICODE)
-    pattern = re.compile(r'".*?.*?"', re.UNICODE)
-    quoted_terms = []
-    ret = pattern.findall(row)
-    for match in ret:
-        quoted_terms.append(match)
-        row = row.replace(match, '')
-
-    ret = row.split()
-    [ret.append(x) for x in quoted_terms]
-
-    ret = list(map(lambda x: x.replace('"', ''), ret))
-    ret = [x for x in ret if x != '']
-
-    # return as set so that only unique values are included
-    return set(ret)
-
-
-def applyRegEx(currRow):
-    '''
-
-    '''
-    
-    re.findall(r'(.+?(?=\sAND\s|\sand\s|\sOR\s|\sor\s))', currRow)
-    
-    # remove logical operators
-    row = re.sub(r'(\s*NOT)*(\s(N|n)ot\s)*((\s|\W)AND)*((\s|\W)(A|a)nd)*(\sOR)*(\sor)*((\s|\W)NEAR\/(\d|\w|\s|\W))*((\s|\W)NEXT\/\d+)*', ')(', currRow)
-
-    # remove country abbreviation (.de, .ch)
-    row = re.sub(r'(.\w\w\/)', '', row)
-    
-    # remove gender m/w
-    row = re.sub(r'(\w\/\w)', '', row)
-
-    # remove rt|RT - which means retweet
-    row = re.sub(r'("rt|"RT)', '', row)
-
-    # remove strings like field=xxx
-    row = re.sub(r'(:?[a-z]*\s?[a-z]+=["]*?[a-z\s]+["]*)', ' ', row)
-
-    # remove twitter handles (e.g. @rwe_ag)
-    row = re.sub(r'(@\w+.\w+)', '', row)
-
-    # remove special characters
-    row = re.sub(r'[^\u00C0-\u017Fa-zA-Z0-9-"&|,.+_\s]', ' ', row, re.UNICODE)
-
-    return row
-
-
 def removeAbbreviations(query):
     '''
         @summary: remove abbreviations like country codes or gender
@@ -125,41 +51,18 @@ def removeTwitter(query):
     # remove rt|RT - which means retweet
     query = re.sub(r'("rt|"RT)', '', query)
 
-    # print('removeTwitter: ', query)
     return query
 
 
-def findQuoted(query):
-    '''
-        @summary: return all terms which are enclosed by quotes ('" "')
-    '''
-    query = removeSpecialChars(removeAbbreviations(removeParams(removeTwitter(query))))
-    pattern = re.compile(r'".*?.*?"', re.UNICODE)
-
-    foundTerms = []
-    matches = pattern.findall(query)
-    for match in matches:
-        # write the found term into a list
-        term = match.replace('"', '')
-        foundTerms.append(term.strip())
-        # remove it from the query afterwards
-        query = query.replace(match, '')
-    
-    return foundTerms, query
-
-
-def findNonQuoted(remainQuery):
+def findMatches(remainQuery):
     '''
         @summary: return all terms which are not enclosed by quotes (TODO: keep word coherence)
     '''
-    # print('findNonQuoted remainQuery: ', remainQuery)
     query = removeAbbreviations(removeSpecialChars(removeParams(removeTwitter(remainQuery))))
-    # print('Query beforeBefore: ', query)
     query = re.sub(r'(\sAND|\sand|\sNOT|\snot|\sOR|\sor|NEAR\/\d)', ')(', query)
     query = re.sub(r'(\s{2}[0-9][0-9]+){1}', '', query)
     # select everything inside parentheses
     query = '(' + query + ')'
-    # print('Query before: ', query)
     terms = []
     pattern = re.compile(r'\((.*?)\)', re.UNICODE)
     matches = pattern.findall(query)
@@ -174,9 +77,9 @@ def findNonQuoted(remainQuery):
         query = query.replace(match, '')
     
     terms = [t for t in terms if t != '']
+    # return as a set so as to filter out duplicates
     return set(terms)
-    # print(matches)
-    # print('Remaining Query: ', remainQuery)
+
 
 
 if __name__ == '__main__':
@@ -184,20 +87,17 @@ if __name__ == '__main__':
         @summary: main
     '''
 
-    fname = 'Searches_Automotive.csv'
-    # fname = 'Searches_Energy.csv'
+    #fname = 'Searches_Automotive.csv'
+    fname = 'Searches_Energy.csv'
     
     # read the csv file
     with open(fname) as csvfile:
         freader = csv.reader(csvfile)
         for row in freader:
-            print('Keyword: ', row[0])
-            print('Row: ', row[1])
-            # terms, remainQuery = findQuoted(row[1])
-    # terms, remainQuery = findQuoted('((Strom NEAR/3 Preis*) OR Strompreis*) NEAR/5 (*entwicklung* OR *entwickel* OR Prognose* OR steig* OR fall* OR sink* OR Kurs) OR Strompreisentwicklung*')
-            terms = findNonQuoted(row[1])
-            print(terms)
-            # print('keyword: ', row[0])
-            # print("k \t {:^10}   \t {:^25}   \t {:^15}".format("Best point", "Good point", "Worst point"))
-            # print(extractWord(applyRegEx(row[1])))
 
+            terms = findMatches(row[1])
+            lt = list(terms)
+            # add the keyword to the beginning of the list
+            lt.insert(0, 'KW: ' + row[0])
+            # print values tab separated
+            print(*lt, sep='\t')
