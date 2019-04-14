74f96633c21034290d989fb9d04e668a17a75c09
lefko@sartre.com <lfko@Sartre>
2019-04-14 17:37:31 +0200

    work in progress

--- nelder-mead/nelder_mead_beuth.py
+++ nelder-mead/nelder_mead_beuth.py
@@ -8,6 +8,7 @@ import csv
 import numpy as np
 import operator
 import matplotlib.pyplot as plt
+from distutils.command.build import build
 
 
 def readDomain():
@@ -20,28 +21,27 @@ def readDomain():
     with open('domain_data.csv', 'r') as csv_file:
         filereader = csv.reader(csv_file, delimiter='\t')
         for row in filereader:
-            y.append(row[0])
-            x.append(row[1])
-            z.append(row[3])
+            y.append(float(row[0]))
+            x.append(float(row[1]))
+            z.append(float(row[3]))
+            points.append(((float(row[1]), float(row[0])), float(row[3])))
 
-    # myZip = zip(x, y, Z)
+    return y, x, z
 
-    # xx = np.ravel(x); yy = np.ravel(y) ; zz = np.ravel(z)
 
+def plotSimplex(simplex):
+    '''
+        @summary: plots the current simplex
+    '''
+    bgw = np.array([simplex[i][0] for i in range(len(simplex))])
     plt.clf()
-    # plt.contourf(x, y, z)
-    plt.scatter(x, y, c=z)
-    plt.plot(x[0:2], y[0:2], 'ro-')
-    plt.plot(x[1:3], y[1:3], 'ro-')
-    plt.plot([x[2:3], x[0:1]], [y[2:3], y[0:1]], 'ro-')    
-
-    # plt.plot(y[0], '+r')  # first/base point
-    
-    print(buildSimplex((x[0], y[0]), (x[1], y[1]), (x[2], y[2]), z[0], z[1], z[2]))
+    plt.scatter(X, Y, c=Z)
+    plt.plot((bgw[0, 0], bgw[1, 0]), (bgw[0, 1], bgw[1, 1]), '-c')
+    plt.plot((bgw[2, 0], bgw[1, 0]), (bgw[2, 1], bgw[1, 1]), '-c')
+    plt.plot((bgw[0, 0], bgw[2, 0]), (bgw[0, 1], bgw[2, 1]), '-c')
+    plt.plot(bgw[0, 0], bgw[0, 1], '+r')  # first/base point 
     plt.show()
-     
-    # X, Y = np.meshgrid(x, y)
-
+    
                 
 def buildSimplex(A, B, C, fA, fB, fC):
     '''
@@ -51,12 +51,131 @@ def buildSimplex(A, B, C, fA, fB, fC):
         @param fA: value for point A
         @param fB: value for point B
         @param fC: value for point C
+        @return: ordered simplex: Best Point, Good Point, Worst Point
     '''
-    return [(A, fA), (B, fB), (C, fC)]
+    return sorted([(A, fA), (B, fB), (C, fC)], key=operator.itemgetter(1))
+
+"""
+    Nelder-Mead functions
+"""
+
+
+def doReflection(CP, WP, alpha=1):
+    '''
+        @param m: midpoint
+        @param WP: worst point
+        @param alpha: reflection parameter
+        @return: reflected point
+    '''
+    # return (CP[0] - alpha * WP[0], CP[1] - alpha * WP[1])
+    return (2. * CP[0] - WP[0], 2. * CP[1] - WP[1])
+
+
+def doExpansion(CP, RP, gamma=2):
+    '''
+        @param CP: midpoint
+        @param RP: reflection point
+        @param gamma: expansion parameter
+        @return: expanded point
+    '''
+    return (2. * RP[0] - CP[0], 2. * RP[1] - CP[1])
+
+
+def doContraction(m, BS):
+    '''
+        @param points: points, worst one excluded
+        @param x_h: worst point
+        @param beta: contraction parameter
+        @return: contracted point
+    '''    
+    
+    # m = calcCentroid(BS)
+    RP = doReflection(m, BS[dim][0])
+    WP = BS[2][0]
+    C1P = ((RP[0] + m[0]) / 2., (RP[1] + m[1]) / 2.)
+    C2P = ((WP[0] + m[0]) / 2., (WP[1] + m[1]) / 2.)
+    if f(C1P[0], C1P[1]) <= f(C2P[0], C2P[1]):
+        return C1P
+    return C2P
+
+
+def doCompression(BS):
+    '''
+        @summary: nelder-mead compression
+        @param BS: current BestSimplex
+    '''
+    return calcCentroid(BS), calcCentroid(BS, P_1=0, P_2=2)
+
+
+def calcCentroid(BP, GP, P_1=0, P_2=1):
+    '''
+        @summary: 
+        @param BS: current BestSimplex
+        @return: midpoint between best and good point
+    '''
+    # compute the sum divided by the number of points
+    return ((BP[0] + GP[0]) / 2., (BP[1] + GP[1]) / 2.)
+
+
+def optimize(BS, iterate=15):
+    '''
+        @summary: the actual optimization
+    '''
+    
+    print("k \t {:^10}   \t {:^10}   \t {:^10}".format("Best point", "Good point", "Worst point"))
+    
+    for _ in range(iterate):
+        # m = calcCentroid(BS) 
+        WP = BS[dim][0]  # worst point coordinates
+        GP = BS[1][0]  # good point coordinates
+        BP = BS[0][0]  # best point coordinates
+        
+        print("{} \t {}   \t {}   \t {}"
+                .format(_ + 1, BS[0][0], BS[1][0], BS[dim][0]))
+        
+        CP = calcCentroid(BP, GP) 
+        RP = doReflection(CP, WP)  # reflection point
+        print('RP: ', RP)
+        print(getValueForPoint(RP), getValueForPoint(GP))
+        if  getValueForPoint(RP) < getValueForPoint(GP):
+            print('case (I)')
+            print(getValueForPoint(RP), getValueForPoint(BP))
+            if getValueForPoint(BP) > getValueForPoint(RP):
+                BS = buildSimplex(BP, GP, RP, getValueForPoint(BP), getValueForPoint(GP), getValueForPoint(RP))
+                print('new BestSimplex: ', BS)
+            
+            else:    
+                EP = doExpansion(CP, RP)  # expansion point
+                print('EP ', EP)
+                print(getValueForPoint(EP))
+
+                if getValueForPoint(RP) < getValueForPoint(BP):
+                    BS = buildSimplex(BP, GP, EP, getValueForPoint(BP), getValueForPoint(GP), getValueForPoint(EP))
+                else:
+                    BS = buildSimplex(BP, GP, RP, getValueForPoint(BP), getValueForPoint(GP), getValueForPoint(RP))
+    
+                print('new BestSimplex: ', BS)
+        
+    print('final Simplex: ', BS)
+    plt.show()
+
+
+def getValueForPoint(pt):
+    '''
+    '''
+    for point in points:
+        if pt in point:
+            return point[1]
 
 
 if __name__ == '__main__':
     '''
     '''
-    bestSimplex = []
-    readDomain()
+    points = []
+    dim = 2
+    Y, X, Z = readDomain()
+    bestSimplex = buildSimplex((X[0], Y[0]), (X[1], Y[1]), (X[2], Y[2]), Z[0], Z[1], Z[2])
+    # plotSimplex(bestSimplex)
+    
+    optimize(bestSimplex, 5)
+    
